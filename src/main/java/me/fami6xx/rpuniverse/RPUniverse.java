2f10c14f43b39c144a7906fc8d23bb12087dba20
Fami6Xx <libojer@gmail.com>
2023-10-07 17:32:58 +0200

    feat: innit

---
+++ src/main/java/me/fami6xx/rpuniverse/RPUniverse.java
@@ -0,0 +1,17 @@
+package me.fami6xx.rpuniverse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPUniverse extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
