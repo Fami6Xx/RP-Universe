06be3089fa7d8d70962b90be0953297f524f7900
Fami6Xx <libojer@gmail.com>
2023-10-24 09:26:28 +0200

    feat: datasystem in main class

--- src/main/java/me/fami6xx/rpuniverse/RPUniverse.java
+++ src/main/java/me/fami6xx/rpuniverse/RPUniverse.java
@@ -1,18 +1,25 @@
 package me.fami6xx.rpuniverse;
 
+import me.fami6xx.rpuniverse.core.DataSystem;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RPUniverse extends JavaPlugin {
+    private DataSystem dataSystem;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        dataSystem = new DataSystem();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        dataSystem.shutdown();
+    }
+
+    public DataSystem getDataSystem() {
+        return dataSystem;
     }
 
     public static RPUniverse getInstance() {
