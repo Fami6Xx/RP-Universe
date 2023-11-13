3bb9c7eb5e83c99c200c7d1d28b414a4a3f76909
Fami6Xx <libojer@gmail.com>
2023-11-13 08:57:05 +0100

    feat: implemented HoloApi

--- src/main/java/me/fami6xx/rpuniverse/RPUniverse.java
+++ src/main/java/me/fami6xx/rpuniverse/RPUniverse.java
@@ -1,25 +1,38 @@
 package me.fami6xx.rpuniverse;
 
 import me.fami6xx.rpuniverse.core.DataSystem;
+import me.fami6xx.rpuniverse.core.holoapi.HoloAPI;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RPUniverse extends JavaPlugin {
     private DataSystem dataSystem;
+    private HoloAPI holoAPI;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         dataSystem = new DataSystem();
+        holoAPI = new HoloAPI();
+
+        if(!holoAPI.enable()){
+            getLogger().severe("HolographicDisplays or ProtocolLib is not installed! Disabling plugin...");
+            getServer().getPluginManager().disablePlugin(this);
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
         dataSystem.shutdown();
+        holoAPI.disable();
     }
 
     public DataSystem getDataSystem() {
         return dataSystem;
+    }
+
+    public HoloAPI getHoloAPI() {
+        return holoAPI;
     }
 
     public static RPUniverse getInstance() {
