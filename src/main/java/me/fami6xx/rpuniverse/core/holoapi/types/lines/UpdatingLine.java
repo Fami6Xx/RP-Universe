080bcd1ff12db604f0c964151340fc92fcd98a7e
Fami6Xx <libojer@gmail.com>
2023-11-28 16:10:00 +0200

    feat: updated UpdatingLine to DecentHolograms

--- src/main/java/me/fami6xx/rpuniverse/core/holoapi/types/lines/UpdatingLine.java
+++ src/main/java/me/fami6xx/rpuniverse/core/holoapi/types/lines/UpdatingLine.java
@@ -1,22 +1,23 @@
 package me.fami6xx.rpuniverse.core.holoapi.types.lines;
 
 import com.gmail.filoghost.holographicdisplays.api.line.TextLine;
+import eu.decentsoftware.holograms.api.holograms.HologramLine;
 import me.fami6xx.rpuniverse.RPUniverse;
 import org.bukkit.scheduler.BukkitRunnable;
 
 
 public abstract class UpdatingLine {
-    private final TextLine line;
+    private final HologramLine line;
 
     private final long period;
 
-    public UpdatingLine(TextLine textLine){
+    public UpdatingLine(HologramLine textLine){
         this.line = textLine;
         this.period = 20;
         start();
     }
 
-    public UpdatingLine(TextLine textLine, long period){
+    public UpdatingLine(HologramLine textLine, long period){
         this.line = textLine;
         this.period = period;
         start();
@@ -28,7 +29,7 @@ public abstract class UpdatingLine {
         new BukkitRunnable(){
             @Override
             public void run(){
-                if(!line.getParent().isDeleted()) {
+                if(!line.isDisabled()) {
                     String update = update();
                     if (!line.getText().equals(update)) {
                         line.setText(update);
