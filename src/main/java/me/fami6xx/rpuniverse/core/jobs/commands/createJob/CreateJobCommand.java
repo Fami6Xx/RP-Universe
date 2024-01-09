7e3fc87dc0f9c2b0d7363e4798b31316b2f2d639
Fami6Xx <libojer@gmail.com>
2024-01-09 21:44:51 +0100

    refactor: remove unnecessary PlayerQuitEvent import
    The PlayerQuitEvent import was removed from the CreateJobCommand class as it was not being used in the code, preventing potential confusion and maintaining a clean codebase.

--- src/main/java/me/fami6xx/rpuniverse/core/jobs/commands/createJob/CreateJobCommand.java
+++ src/main/java/me/fami6xx/rpuniverse/core/jobs/commands/createJob/CreateJobCommand.java
@@ -12,7 +12,6 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
