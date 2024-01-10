ce47503b994bf7e99a19d0bbdd5a7d05bed535bd
Fami6Xx <libojer@gmail.com>
2024-01-10 11:13:19 +0100

    feat: add new Position class
    The new Position class includes attributes such as name, salary, working step permission level, boss status, and default flag. It includes getter and setter methods for each attribute, as well as toString and fromString methods for easy representation and conversion of Position objects.

---
+++ src/main/java/me/fami6xx/rpuniverse/core/jobs/classes/Position.java
@@ -0,0 +1,166 @@
+package me.fami6xx.rpuniverse.core.jobs.classes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Position {
+    private String name;
+    private int salary;
+    private int workingStepPermissionLevel;
+    private boolean isBoss;
+    private boolean isDefault;
+
+    /**
+     * Position full-argument constructor
+     *
+     * @param name Name of the position
+     * @param salary Salary of the position
+     * @param workingStepPermissionLevel Working step permission level of the position
+     * @param isBoss Boolean flag if position is a boss
+     * @param isDefault Boolean flag if position is default
+     */
+    public Position(String name, int salary, int workingStepPermissionLevel, boolean isBoss, boolean isDefault) {
+        this.name = name;
+        this.salary = salary;
+        this.workingStepPermissionLevel = workingStepPermissionLevel;
+        this.isBoss = isBoss;
+        this.isDefault = isDefault;
+    }
+
+    /**
+     * Getter method for name
+     * @return Name of the position
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter method for name
+     * @param name New name of the position
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Getter method for salary
+     * @return Salary of the position
+     */
+    public int getSalary() {
+        return salary;
+    }
+
+    /**
+     * Setter method for salary
+     * @param salary New salary of the position
+     */
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    /**
+     * Getter method for working step permission level
+     * @return Working step permission level of the position
+     */
+    public int getWorkingStepPermissionLevel() {
+        return workingStepPermissionLevel;
+    }
+
+    /**
+     * Setter method for working step permission level
+     * @param workingStepPermissionLevel New working step permission level of the position
+     */
+    public void setWorkingStepPermissionLevel(int workingStepPermissionLevel) {
+        this.workingStepPermissionLevel = workingStepPermissionLevel;
+    }
+
+    /**
+     * Getter method for isBoss
+     * @return If the position is a boss position
+     */
+    public boolean isBoss() {
+        return isBoss;
+    }
+
+    /**
+     * Setter method for isBoss
+     * @param boss If the position is a boss position
+     */
+    public void setBoss(boolean boss) {
+        isBoss = boss;
+    }
+
+    /**
+     * Getter method for isDefault
+     * @return If the position is a default position
+     */
+    public boolean isDefault() {
+        return isDefault;
+    }
+
+    /**
+     * Setter method for isDefault
+     * @param aDefault If the position is a default position
+     */
+    public void setDefault(boolean aDefault) {
+        isDefault = aDefault;
+    }
+
+    /**
+     * Returns a string representation of the Position object.
+     *
+     * @return A string representation of the Position object, including the name, salary,
+     *         working step permission level, boss flag, and default flag.
+     */
+    @Override
+    public String toString() {
+        return "Position{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", workingStepPermissionLevel=" + workingStepPermissionLevel +
+                ", isBoss=" + isBoss +
+                ", isDefault=" + isDefault +
+                '}';
+    }
+
+    /**
+     * Constructs a Position object from a string representation.
+     *
+     * @param s The string representation of the Position object.
+     *          The string should be in the format "{key1=value1, key2=value2, ...}".
+     *          The following keys are expected:
+     *          - name: Name of the position (String)
+     *          - salary: Salary of the position (int)
+     *          - workingStepPermissionLevel: Working step permission level of the position (int)
+     *          - isBoss: Boolean flag if position is a boss (boolean)
+     *          - isDefault: Boolean flag if position is default (boolean)
+     *
+     * @return A Position object created from the given string representation,
+     *         or null if any error occurred during the conversion.
+     */
+    public static Position fromString(String s) {
+        try {
+            s = s.substring(s.indexOf('{') + 1, s.lastIndexOf('}'));
+            String[] keyValuePairs = s.split(", ");
+            Map<String, String> map = new HashMap<>();
+
+            for (String pair : keyValuePairs) {
+                String[] entry = pair.split("=");
+                map.put(entry[0].trim(), entry[1].trim().replaceAll("'", ""));
+            }
+
+            String name = map.get("name");
+            int salary = Integer.parseInt(map.get("salary"));
+            int workingStepPermissionLevel = Integer.parseInt(map.get("workingStepPermissionLevel"));
+            boolean isBoss = Boolean.parseBoolean(map.get("isBoss"));
+            boolean isDefault = Boolean.parseBoolean(map.get("isDefault"));
+
+            return new Position(name, salary, workingStepPermissionLevel, isBoss, isDefault);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
