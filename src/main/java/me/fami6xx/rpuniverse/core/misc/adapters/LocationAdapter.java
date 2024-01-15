6ed6b7124bcc19c4841dd8c828ceebb1a65c94bd
Fami6Xx <libojer@gmail.com>
2024-01-15 08:45:06 +0100

    feat: add LocationAdapter for JSON serialization
    A new class, LocationAdapter, has been added to handle JSON serialization and deserialization of the Location objects. This adapter converts each property of the Location object into JSON format and vice versa, allowing efficient storage and retrieval of object data.

---
+++ src/main/java/me/fami6xx/rpuniverse/core/misc/adapters/LocationAdapter.java
@@ -0,0 +1,33 @@
+package me.fami6xx.rpuniverse.core.misc.adapters;
+
+import com.google.gson.*;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+import java.lang.reflect.Type;
+
+public class LocationAdapter implements JsonSerializer<Location>, JsonDeserializer<Location> {
+    @Override
+    public JsonElement serialize(Location src, Type typeOfSrc, JsonSerializationContext context) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("world", src.getWorld().getName());
+        jsonObject.addProperty("x", src.getX());
+        jsonObject.addProperty("y", src.getY());
+        jsonObject.addProperty("z", src.getZ());
+        jsonObject.addProperty("yaw", src.getYaw());
+        jsonObject.addProperty("pitch", src.getPitch());
+        return jsonObject;
+    }
+
+    @Override
+    public Location deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+        String world = jsonObject.get("world").getAsString();
+        double x = jsonObject.get("x").getAsDouble();
+        double y = jsonObject.get("y").getAsDouble();
+        double z = jsonObject.get("z").getAsDouble();
+        float yaw = jsonObject.get("yaw").getAsFloat();
+        float pitch = jsonObject.get("pitch").getAsFloat();
+        return new Location(Bukkit.getWorld(world), x, y, z, yaw, pitch);
+    }
+}
