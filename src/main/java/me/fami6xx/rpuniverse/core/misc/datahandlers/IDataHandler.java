86998492ce6286871c2e07d44804dbe1af837748
Fami6Xx <libojer@gmail.com>
2024-01-15 09:17:47 +0100

    feat: add job data handling in IDataHandler interface
    Additional methods were implemented in the IDataHandler interface and its implementation in JSONDataHandler for retrieving and saving job data. It includes changing the name of the data directory to differentiate between player and job data directories.

--- src/main/java/me/fami6xx/rpuniverse/core/misc/datahandlers/IDataHandler.java
+++ src/main/java/me/fami6xx/rpuniverse/core/misc/datahandlers/IDataHandler.java
@@ -1,5 +1,6 @@
 package me.fami6xx.rpuniverse.core.misc.datahandlers;
 
+import me.fami6xx.rpuniverse.core.jobs.Job;
 import me.fami6xx.rpuniverse.core.misc.PlayerData;
 
 public interface IDataHandler {
@@ -35,6 +36,24 @@ public interface IDataHandler {
      * @return true if the data was saved successfully, false otherwise.
      */
     boolean savePlayerData(PlayerData data);
+
+    /**
+     * Retrieves job data for a given name. The data is serialized and deserialized
+     * by the selected Data Handler.
+     *
+     * @param name The name of the job to retrieve data for.
+     * @return The job data for the specified name.
+     */
+    Job getJobData(String name);
+
+    /**
+     * Saves the job data for a given name. The data is serialized and deserialized by the selected Data Handler.
+     *
+     * @param name The name of the job to save data for.
+     * @param data The job data to save.
+     * @return true if the data was saved successfully, false otherwise.
+     */
+    boolean saveJobData(String name, String data);
 
     /**
      * Called at the start of the plugin, this is used to determine how often the save task should run.
--- src/main/java/me/fami6xx/rpuniverse/core/misc/datahandlers/JSONDataHandler.java
+++ src/main/java/me/fami6xx/rpuniverse/core/misc/datahandlers/JSONDataHandler.java
@@ -3,6 +3,7 @@ package me.fami6xx.rpuniverse.core.misc.datahandlers;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import me.fami6xx.rpuniverse.RPUniverse;
+import me.fami6xx.rpuniverse.core.jobs.Job;
 import me.fami6xx.rpuniverse.core.misc.PlayerData;
 
 import java.util.logging.Logger;
@@ -12,14 +13,15 @@ import java.nio.file.Paths;
 
 public class JSONDataHandler implements IDataHandler {
     private Gson gson;
-    private final Path dataDirectory = Paths.get(RPUniverse.getInstance().getDataFolder().getPath() + "/playerdata/");
+    private final Path playerDataDirectory = Paths.get(RPUniverse.getInstance().getDataFolder().getPath() + "/playerdata/");
+    private final Path jobDataDirectory = Paths.get(RPUniverse.getInstance().getDataFolder().getPath() + "/jobs/");
     private final Logger logger =  Logger.getLogger(JSONDataHandler.class.getName());
 
     @Override
     public boolean startUp() {
         try {
             this.gson = new GsonBuilder().setPrettyPrinting().create();
-            File dataDir = new File(dataDirectory.toUri());
+            File dataDir = new File(playerDataDirectory.toUri());
             if(!dataDir.exists()){
                 return dataDir.mkdirs();
             }
@@ -43,7 +45,7 @@ public class JSONDataHandler implements IDataHandler {
 
     @Override
     public PlayerData loadPlayerData(String uuid) {
-        Path playerFilePath = dataDirectory.resolve(uuid + ".json");
+        Path playerFilePath = playerDataDirectory.resolve(uuid + ".json");
         try (Reader reader = new FileReader(playerFilePath.toFile())) {
             return gson.fromJson(reader, PlayerData.class);
         } catch (IOException e) {
@@ -54,8 +56,32 @@ public class JSONDataHandler implements IDataHandler {
 
     @Override
     public boolean savePlayerData(PlayerData data) {
-        Path playerFilePath = dataDirectory.resolve(data.getUuid() + ".json");
+        Path playerFilePath = playerDataDirectory.resolve(data.getUuid() + ".json");
         try (Writer writer = new FileWriter(playerFilePath.toFile())) {
+            gson.toJson(data, writer);
+            return true;
+        } catch (IOException e) {
+            logger.severe(e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public Job getJobData(String name) {
+        Path jobFilePath = jobDataDirectory.resolve(name + ".json");
+
+        try (Reader reader = new FileReader(jobFilePath.toFile())) {
+            return gson.fromJson(reader, Job.class);
+        } catch (IOException e) {
+            logger.severe(e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public boolean saveJobData(String name, String data) {
+        Path jobFilePath = jobDataDirectory.resolve(name + ".json");
+        try (Writer writer = new FileWriter(jobFilePath.toFile())) {
             gson.toJson(data, writer);
             return true;
         } catch (IOException e) {
