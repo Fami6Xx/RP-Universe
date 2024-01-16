7f0235f97c1f140fb4b63dc981341e65b6f73c0d
Fami6Xx <libojer@gmail.com>
2024-01-16 07:40:50 +0100

    refactor: remove unused import in Menu.java
    The java.util.ArrayList import in Menu.java was unused, so it has been removed to maintain clean and efficient code. Ensuring the absence of unused imports aids in the program's overall performance and maintainability.

--- src/main/java/me/fami6xx/rpuniverse/core/menuapi/types/Menu.java
+++ src/main/java/me/fami6xx/rpuniverse/core/menuapi/types/Menu.java
@@ -11,7 +11,6 @@ import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
