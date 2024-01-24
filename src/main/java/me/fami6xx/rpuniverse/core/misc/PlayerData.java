0e6426995f7c330eea70ade83bb1c8976cbd7a9b
Fami6Xx <libojer@gmail.com>
2024-01-24 09:34:28 +0100

    feat: add player data handling in PlayerData class
    Updated PlayerData class to include functionality for handling and storing player data. Changes include the addition of offline player handling, job selection, and player specific job list. Integrated methods for data preparation before saving and loading after saving, as well as getter methods for data retrieval.

--- src/main/java/me/fami6xx/rpuniverse/core/misc/PlayerData.java
+++ src/main/java/me/fami6xx/rpuniverse/core/misc/PlayerData.java
@@ -1,7 +1,13 @@
 package me.fami6xx.rpuniverse.core.misc;
 
+import me.fami6xx.rpuniverse.RPUniverse;
+import me.fami6xx.rpuniverse.core.jobs.Job;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -12,17 +18,120 @@ import java.util.UUID;
  * To ignore a field from being serialized, use the transient keyword.
  */
 public class PlayerData {
-    transient Player bindedPlayer;
+    private transient Player bindedPlayer;
+    private transient OfflinePlayer bindedOfflinePlayer;
+    private transient List<Job> playerJobs = new ArrayList<>();
+    private transient Job selectedPlayerJob = null;
 
-    // Some testing data
-    public int testInt = 0;
-    public String testString = "Hello World!";
-    private transient String testTransientString = "This string is transient and will not be saved.";
-    private int hunger = 20;
-    private int health = 20;
-    private UUID uuid = UUID.randomUUID();
+    private final UUID dataUUID;
+    private String playerUUID;
+    private String selectedJobName;
+    private List<String> playerJobNames;
 
-    public UUID getUuid() {
-        return uuid;
+    public PlayerData(Player bindedPlayer) {
+        this.bindedPlayer = bindedPlayer;
+        this.bindedOfflinePlayer = null;
+        this.dataUUID = UUID.randomUUID();
+    }
+
+    public PlayerData(OfflinePlayer bindedOfflinePlayer) {
+        this.bindedOfflinePlayer = bindedOfflinePlayer;
+        this.bindedPlayer = null;
+        this.dataUUID = UUID.randomUUID();
+    }
+
+    /**
+     * Prepares the player data for saving.
+     * <p>
+     * If the bindedPlayer field is not null, it retrieves the name of the player and assigns it to playerName.
+     * <p>
+     * If the bindedOfflinePlayer field is not null, it retrieves the name of the offline player and assigns it to playerName.
+     * <p>
+     * It retrieves the UUID of the player data as a String and assigns it to playerUUID.
+     * <p>
+     * If the selectedPlayerJob field is not null, it retrieves the name of the job and assigns it to selectedJobName.
+     * <p>
+     * Initializes playerJobNames as an empty ArrayList.
+     * <p>
+     * For each job in playerJobs, it retrieves the name of the job and adds it to playerJobNames.
+     */
+    public void prepareForSave(){
+        playerUUID = getPlayerUUID().toString();
+
+        selectedJobName = selectedPlayerJob == null ? null : selectedPlayerJob.getName();
+
+        playerJobNames = new ArrayList<>();
+        for(Job job : playerJobs)
+            playerJobNames.add(job.getName());
+    }
+
+    /**
+     * Loads the player data after it has been saved.
+     * <p>
+     * If the bindedPlayer field is not null, it retrieves the player object using the playerUUID and assigns it to bindedPlayer.
+     * <p>
+     * If the bindedOfflinePlayer field is not null, it retrieves the offline player object using the playerUUID and assigns it to bindedOfflinePlayer.
+     * <p>
+     * If the selectedJobName field is not null, it retrieves the job object using the selectedJobName and assigns it to selectedPlayerJob.
+     * <p>
+     * Initializes playerJobs as an empty ArrayList.
+     * <p>
+     * For each jobName in playerJobNames, it retrieves the job object using the jobName and adds it to playerJobs.
+     */
+    public void loadAfterSave(){
+        if(bindedPlayer != null)
+            bindedPlayer = RPUniverse.getInstance().getServer().getPlayer(playerUUID);
+        else if(bindedOfflinePlayer != null)
+            bindedOfflinePlayer = RPUniverse.getInstance().getServer().getOfflinePlayer(UUID.fromString(playerUUID));
+
+        if(selectedJobName != null)
+            selectedPlayerJob = Job.getJob(selectedJobName);
+
+        playerJobs = new ArrayList<>();
+        for(String jobName : playerJobNames)
+            playerJobs.add(Job.getJob(jobName));
+    }
+
+    /**
+     * Retrieves the UUID of the player data.
+     *
+     * @return the UUID of the player data*/
+    public UUID getDataUUID() {
+        return dataUUID;
+    }
+
+    /**
+     * Retrieves the UUID of the player data.
+     *
+     * @return the UUID of the player data
+     */
+    public UUID getPlayerUUID() {
+        if (bindedPlayer != null) {
+            return bindedPlayer.getUniqueId();
+        } else if (bindedOfflinePlayer != null) {
+            return bindedOfflinePlayer.getUniqueId();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Retrieves the player object that is currently bound to this PlayerData instance.
+     *
+     * @return the player object that is currently bound, or null if no player is bound.
+     */
+    @Nullable
+    public Player getBindedPlayer() {
+        return bindedPlayer;
+    }
+
+    /**
+     * Returns the OfflinePlayer object that is currently bound to this PlayerData instance.
+     *
+     * @return the OfflinePlayer object that is currently bound, or null if no OfflinePlayer is bound.
+     */
+    @Nullable
+    public OfflinePlayer getBindedOfflinePlayer() {
+        return bindedOfflinePlayer;
     }
 }
