b6b8630df90656de1776335082069a6fd4fce59e
Fami6Xx <libojer@gmail.com>
2024-02-04 22:20:58 +0100

    refactor: remove ToDo comment in DoCommand, StatusCommand, and MeCommand
    The 'ToDo' comment representing the need to cap the length of messages in DoCommand, StatusCommand, and MeCommand has been removed. This signifies that restrictions on message length have been successfully implemented for these commands in the project's code.

--- src/main/java/me/fami6xx/rpuniverse/core/commands/MeCommand.java
+++ src/main/java/me/fami6xx/rpuniverse/core/commands/MeCommand.java
@@ -11,7 +11,6 @@ import org.bukkit.entity.Player;
 import java.util.HashMap;
 
 public class MeCommand implements CommandExecutor {
-    // ToDo: Add cap for how long the message can be
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(!(sender instanceof Player)){
