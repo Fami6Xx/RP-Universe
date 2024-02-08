bad65a4ae94330ee1fa0e794bce02c5fc1d01a8b
Fami6Xx <libojer@gmail.com>
2024-02-08 23:45:29 +0100

    feat: add ActionBarHandler enabling player message cycling
    Implemented a new class, ActionBarHandler, to manage and display message updates for players. This class provides methods for adding and removing players, as well as adding messages to a player's action bar, allowing forced immediate display or periodic cycling of queued messages.

---
+++ src/main/java/me/fami6xx/rpuniverse/core/misc/basichandlers/ActionBarHandler.java
@@ -0,0 +1,107 @@
+package me.fami6xx.rpuniverse.core.misc.basichandlers;
+
+import me.fami6xx.rpuniverse.RPUniverse;
+import me.fami6xx.rpuniverse.core.misc.utils.FamiUtils;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.*;
+
+public class ActionBarHandler {
+    private final Map<Player, Queue<String>> playerMessages = new HashMap<>();
+
+    /**
+     * Add a player to the playerMessages map and start cycling messages for that player.
+     *
+     * @param player   The player to add.
+     * @param messages The list of messages for the player.
+     */
+    public void addPlayer(Player player, List<String> messages) {
+        Queue<String> messageQueue = new LinkedList<>(messages);
+        playerMessages.put(player, messageQueue);
+        cycleMessages(player);
+    }
+
+    /**
+     * Remove a player from the playerMessages map.
+     *
+     * @param player The player to be removed.
+     */
+    public void removePlayer(Player player) {
+        playerMessages.remove(player);
+    }
+
+    /**
+     * Add a message to the player's action bar.
+     * This method will add the given message to the action bar of the specified player.
+     * If the player is not currently displaying any messages, the action bar will start cycling through the messages periodically.
+     *
+     * @param player The player to add the message for.
+     * @param message The message to add.
+     * @param force Whether to immediately display the message on the action bar.
+     * @param once Whether to add the message to the queue if force is true.
+     */
+    public void addMessage(Player player, String message, boolean force, boolean once) {
+        Queue<String> messages = playerMessages.get(player);
+        if (messages != null) {
+            if (force) {
+                player.sendActionBar(FamiUtils.format(message));
+                if (!once) {
+                    messages.add(message);
+                }
+            } else {
+                messages.add(message);
+            }
+        }else{
+            List<String> messagesList = new ArrayList<>();
+            messagesList.add(message);
+            this.addPlayer(player, messagesList);
+        }
+    }
+
+    /**
+     * Add a message to the player's action bar.
+     * This method will add the given message to the action bar of the specified player.
+     * If the player is not currently displaying any messages, the action bar will start cycling through the messages periodically.
+     *
+     * @param player The player to add the message for.
+     * @param message The message to add.
+     * @param force Whether to immediately display the message on the action bar.
+     */
+    public void addMessage(Player player, String message, boolean force) {
+        this.addMessage(player, message, force, false);
+    }
+
+    /**
+     * Add a message to the player's action bar.
+     * This method will add the given message to the action bar of the specified player.
+     * If the player is not currently displaying any messages, the action bar will start cycling through the messages periodically.
+     *
+     * @param player The player to add the message for.
+     * @param message The message to add.
+     */
+    public void addMessage(Player player, String message) {
+        this.addMessage(player, message, false, false);
+    }
+
+    /**
+     * Cycle through the messages for the given player and display them on the action bar periodically.
+     *
+     * @param player The player for whom to cycle the messages.
+     */
+    private void cycleMessages(Player player) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                Queue<String> messages = playerMessages.get(player);
+                if (messages != null && !messages.isEmpty()) {
+                    String message = messages.poll();
+                    player.sendActionBar(FamiUtils.format(message));
+                    messages.add(message);
+                } else {
+                    this.cancel();
+                }
+            }
+        }.runTaskTimer(RPUniverse.getInstance(), 0L, 20L);
+    }
+}
