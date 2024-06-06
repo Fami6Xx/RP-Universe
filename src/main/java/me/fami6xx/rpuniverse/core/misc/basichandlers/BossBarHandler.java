ab7025445a121c00913fe82b1358133876137648
Fami6Xx <libojer@gmail.com>
2024-06-06 11:25:42 +0200

    feat: added javadocs to BossBarHandler

--- src/main/java/me/fami6xx/rpuniverse/core/misc/basichandlers/BossBarHandler.java
+++ src/main/java/me/fami6xx/rpuniverse/core/misc/basichandlers/BossBarHandler.java
@@ -13,15 +13,33 @@ import org.bukkit.entity.Player;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The BossBarHandler is the main class for handling the boss bars.
+ * It is responsible for adding, removing, and updating the boss bars.
+ * <p>
+ * The BossBarHandler is a singleton class, so only one instance of it should be created.
+ * <p>
+ * The BossBarHandler is also a listener for the PlayerJoinEvent and PlayerQuitEvent.
+ * This means that the BossBarHandler will also listen for these events and update the boss bars accordingly.
+ */
 public class BossBarHandler {
     private final Map<Player, BossBar> bossBars = new HashMap<>();
 
+    /**
+     * Add a player to the boss bar
+     * @param player The player to add
+     * @param message The message to display on the boss bar
+     */
     public void addPlayer(Player player, String message) {
         BossBar bossBar = Bukkit.createBossBar(message, getBarColor(), BarStyle.SOLID);
         bossBar.addPlayer(player);
         bossBars.put(player, bossBar);
     }
 
+    /**
+     * Remove a player from the boss bar
+     * @param player The player to remove
+     */
     public void removePlayer(Player player) {
         BossBar bossBar = bossBars.remove(player);
         if (bossBar != null) {
@@ -29,6 +47,11 @@ public class BossBarHandler {
         }
     }
 
+    /**
+     * Set the message of a player's boss bar
+     * @param player The player to set the message of
+     * @param message The message to set the boss bar to
+     */
     public void setMessage(Player player, String message) {
         BossBar bossBar = bossBars.get(player);
         if (bossBar != null) {
@@ -39,6 +62,10 @@ public class BossBarHandler {
         addPlayer(player, message);
     }
 
+    /**
+     * Update the boss bar of a player
+     * @param player The player to update the boss bar of
+     */
     public void updateBossBar(Player player){
         String message = "";
         PlayerData playerData = RPUniverse.getPlayerData(player.getUniqueId().toString());
@@ -58,6 +85,10 @@ public class BossBarHandler {
         this.setMessage(player, FamiUtils.format(RPUniverse.getLanguageHandler().bossBarColorBeforeJob + message));
     }
 
+    /**
+     * Get the bar color from the config.yml
+     * @return The bar color
+     */
     private static BarColor getBarColor(){
         String string = RPUniverse.getInstance().getConfiguration().getString("general.bossBarColor");
         if(string == null){
