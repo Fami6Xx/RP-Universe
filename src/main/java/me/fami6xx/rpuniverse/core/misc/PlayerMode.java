280f0e3889329cdf1cc6916f9f9eb40c7240ba01
Fami6Xx <libojer@gmail.com>
2024-06-06 10:56:22 +0200

    feat: added javadocs to PlayerMode enum class

--- src/main/java/me/fami6xx/rpuniverse/core/misc/PlayerMode.java
+++ src/main/java/me/fami6xx/rpuniverse/core/misc/PlayerMode.java
@@ -1,5 +1,10 @@
 package me.fami6xx.rpuniverse.core.misc;
 
+/**
+ * The PlayerMode enum is used to determine the mode of the player
+ * @author Fami6xx
+ * @version 1.0
+ */
 public enum PlayerMode {
     USER("user"),
     MODERATOR("moderator"),
@@ -11,10 +16,19 @@ public enum PlayerMode {
         this.mode = mode;
     }
 
+    /**
+     * Get the string of the PlayerMode
+     * @return The string of the PlayerMode
+     */
     public String getMode(){
         return mode;
     }
 
+    /**
+     * Get the PlayerMode from a string
+     * @param mode The string to get the PlayerMode from
+     * @return The PlayerMode if found, null otherwise
+     */
     public static PlayerMode getModeFromString(String mode){
         for(PlayerMode playerMode : PlayerMode.values()){
             if(playerMode.getMode().equalsIgnoreCase(mode)) return playerMode;
