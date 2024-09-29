795609152e1a090056004e925c8df4e9ef2a6e04
Fami6Xx <libojer@gmail.com>
2024-09-29 12:21:29 +0200

    feat: added function for lock (getOwnersAsString)

--- src/main/java/me/fami6xx/rpuniverse/core/locks/Lock.java
+++ src/main/java/me/fami6xx/rpuniverse/core/locks/Lock.java
@@ -2,12 +2,15 @@ package me.fami6xx.rpuniverse.core.locks;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import javax.annotation.Nullable;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 import me.fami6xx.rpuniverse.core.jobs.Job;
@@ -97,6 +100,31 @@ public class Lock {
      */
     public UUID getUUID() {
         return uuid;
+    }
+
+    /**
+     * Get the owners of the lock as a string
+     * @return The owners of the lock as a string
+     */
+    public String getOwnersAsString() {
+        StringBuilder owners = new StringBuilder();
+        if (this.getOwners() == null || this.getOwners().isEmpty()) {
+            owners = new StringBuilder("None");
+        } else if (this.getOwners().size() > 1){
+            for (int i = 0; i < this.getOwners().size(); i++) {
+                String uuid = this.getOwners().get(i);
+                OfflinePlayer offlineOwner = Bukkit.getOfflinePlayer(UUID.fromString(uuid));
+                owners.append(offlineOwner.getName());
+                if (i < this.getOwners().size() - 1) {
+                    owners.append(", ");
+                }
+            }
+        } else {
+            OfflinePlayer offlineOwner = Bukkit.getOfflinePlayer(UUID.fromString(this.getOwners().get(0)));
+            owners = new StringBuilder(Objects.requireNonNull(offlineOwner.getName()));
+        }
+
+        return owners.toString();
     }
 }
 
