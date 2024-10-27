028378ad725d48f99cb15bc4465d41b667631bb4
Fami6Xx <libojer@gmail.com>
2024-10-27 13:38:46 +0100

    feat: working step from & to string updated

--- src/main/java/me/fami6xx/rpuniverse/core/jobs/WorkingStep.java
+++ src/main/java/me/fami6xx/rpuniverse/core/jobs/WorkingStep.java
@@ -10,6 +10,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -21,6 +22,7 @@ import java.util.stream.Collectors;
 public class WorkingStep {
     private static final Logger LOGGER = RPUniverse.getInstance().getLogger();
 
+    private UUID uuid = UUID.randomUUID();
     private List<Location> workingLocations;
     private int timeForStep; // In ticks
     private ItemStack itemNeeded; // Can be null
@@ -47,6 +49,28 @@ public class WorkingStep {
         this.itemGiven = itemGiven;
         this.amountOfItemGiven = amountOfItemGiven;
         this.neededPermissionLevel = neededPermissionLevel;
+    }
+
+    /**
+     * A working step is a step that the player must do to receive an item. The player must have a specific item in his inventory to do the step.
+     * @param workingLocations The location where the player must be to do the step.
+     * @param timeForStep The time in ticks that the player must be at the location to do the step.
+     * @param itemNeeded The item that the player must have in his inventory to do the step.
+     * @param amountOfItemNeeded The amount of the item that the player must have in his inventory to do the step.
+     * @param itemGiven The item that the player will receive when he does the step.
+     * @param amountOfItemGiven The amount of the item that the player will receive when he does the step.
+     * @param neededPermissionLevel The permission level that the player must have to do the step.
+     * @param uuid The unique identifier of the working step.
+     */
+    private WorkingStep(@Nonnull List<Location> workingLocations, int timeForStep, ItemStack itemNeeded, int amountOfItemNeeded, @Nonnull ItemStack itemGiven, int amountOfItemGiven, int neededPermissionLevel, UUID uuid){
+        this.workingLocations = workingLocations;
+        this.timeForStep = timeForStep;
+        this.itemNeeded = itemNeeded;
+        this.amountOfItemNeeded = amountOfItemNeeded;
+        this.itemGiven = itemGiven;
+        this.amountOfItemGiven = amountOfItemGiven;
+        this.neededPermissionLevel = neededPermissionLevel;
+        this.uuid = uuid;
     }
 
     /**
@@ -219,91 +243,39 @@ public class WorkingStep {
      */
     public static WorkingStep fromString(String s) {
         try {
-            String[] parts = s.substring(s.indexOf('{') + 1, s.lastIndexOf('}')).split(", ");
-
-            // Parsing workingLocations
-            List<Location> workingLocations = new ArrayList<>();
-            if (!parts[0].split("=")[1].equals("null")) {
-                String[] locStrings = parts[0].split("=")[1].split(";");
-                for (String locString : locStrings) {
-                    String[] locParts = locString.split(",");
-                    Location loc = new Location(
-                            Bukkit.getWorld(locParts[0]),
-                            Double.parseDouble(locParts[1]),
-                            Double.parseDouble(locParts[2]),
-                            Double.parseDouble(locParts[3]),
-                            Float.parseFloat(locParts[4]),
-                            Float.parseFloat(locParts[5])
-                    );
-                    workingLocations.add(loc);
-                }
-            }
-
-            int timeForStep = Integer.parseInt(parts[1]);
-
-            // Parsing ItemStack for itemNeeded
-            ItemStack itemNeeded = null;
-            if (!parts[2].equals("null")) {
-                YamlConfiguration yaml = new YamlConfiguration();
-                yaml.loadFromString(parts[2]);
-                itemNeeded = yaml.getItemStack("item");
-            }
-
-            int amountOfItemNeeded = Integer.parseInt(parts[3]);
-
-            // Parsing ItemStack for itemGiven
             YamlConfiguration yaml = new YamlConfiguration();
-            yaml.loadFromString(parts[4]);
-            ItemStack itemGiven = yaml.getItemStack("item");
+            yaml.loadFromString(s);
+            UUID uuid = UUID.fromString(yaml.getString("uuid"));
+            List<Location> workingLocations = (List<Location>) yaml.getList("workingLocations");
+            int timeForStep = yaml.getInt("timeForStep");
+            ItemStack itemNeeded = yaml.getItemStack("itemNeeded");
+            int amountOfItemNeeded = yaml.getInt("amountOfItemNeeded");
+            ItemStack itemGiven = yaml.getItemStack("itemGiven");
+            int amountOfItemGiven = yaml.getInt("amountOfItemGiven");
+            int neededPermissionLevel = yaml.getInt("neededPermissionLevel");
 
-            int amountOfItemGiven = Integer.parseInt(parts[5]);
-            int neededPermissionLevel = Integer.parseInt(parts[6]);
-
-            return new WorkingStep(workingLocations, timeForStep, itemNeeded, amountOfItemNeeded, itemGiven, amountOfItemGiven, neededPermissionLevel);
+            return new WorkingStep(workingLocations, timeForStep, itemNeeded, amountOfItemNeeded, itemGiven, amountOfItemGiven, neededPermissionLevel, uuid);
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, "An error occurred while parsing WorkingStep object from string {\n" + s + "\n}, with error: " + e.getMessage());
             return null;
         }
     }
 
+
     @Override
     public String toString() {
         YamlConfiguration yaml = new YamlConfiguration();
-
-        StringBuilder sb = new StringBuilder("WorkingStep{");
-
-        // Serialize Location
-        String locationsString = workingLocations.stream()
-                .map(loc -> loc.getWorld().getName() + "," + loc.getX() + "," + loc.getY() + "," + loc.getZ() + "," + loc.getYaw() + "," + loc.getPitch())
-                .collect(Collectors.joining(";")); // Separating locations with a semicolon
-        sb.append("workingLocations=").append(locationsString).append(", ");
-
-
-        // Serialize timeForStep
-        sb.append(timeForStep).append(", ");
-
-        // Serialize itemNeeded
+        yaml.set("uuid", uuid.toString());
+        yaml.set("workingLocations", workingLocations);
+        yaml.set("timeForStep", timeForStep);
         if (itemNeeded != null) {
-            yaml.set("item", itemNeeded);
-            sb.append(yaml.saveToString()).append(", ");
-        } else {
-            sb.append("null, ");
+            yaml.set("itemNeeded", itemNeeded);
         }
-
-        // Serialize amountOfItemNeeded
-        sb.append(amountOfItemNeeded).append(", ");
-
-        // Serialize itemGiven
-        yaml = new YamlConfiguration();
-        yaml.set("item", itemGiven);
-        sb.append(yaml.saveToString()).append(", ");
-
-        // Serialize amountOfItemGiven and neededPermissionLevel
-        sb.append(amountOfItemGiven).append(", ")
-                .append(neededPermissionLevel);
-
-        sb.append('}');
-
-        return sb.toString();
+        yaml.set("amountOfItemNeeded", amountOfItemNeeded);
+        yaml.set("itemGiven", itemGiven);
+        yaml.set("amountOfItemGiven", amountOfItemGiven);
+        yaml.set("neededPermissionLevel", neededPermissionLevel);
+        return yaml.saveToString();
     }
+
 }
