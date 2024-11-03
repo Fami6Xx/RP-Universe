3ce50412b11eacdcc0ba22b5adcc9e0af578c497
Fami6Xx <libojer@gmail.com>
2024-11-03 22:47:17 +0100

    feat: improved autocomplete for /rpu

--- src/main/java/me/fami6xx/rpuniverse/core/commands/RPUCoreAutoComplete.java
+++ src/main/java/me/fami6xx/rpuniverse/core/commands/RPUCoreAutoComplete.java
@@ -2,12 +2,15 @@ package me.fami6xx.rpuniverse.core.commands;
 
 import me.fami6xx.rpuniverse.RPUniverse;
 import me.fami6xx.rpuniverse.core.jobs.Job;
+import me.fami6xx.rpuniverse.core.misc.PlayerData;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,13 +26,25 @@ public class RPUCoreAutoComplete implements TabCompleter {
             }
             if (args.length == 3) {
                 if (args[0].equalsIgnoreCase("addjob")) {
+                    Player target = Bukkit.getPlayer(args[1]);
+                    if (target == null) {
+                        return new ArrayList<>();
+                    }
+                    PlayerData data = RPUniverse.getPlayerData(target.getUniqueId().toString());
                     return RPUniverse.getInstance().getJobsHandler().getJobs().stream()
+                            .filter(job -> !data.getPlayerJobs().contains(job))
                             .map(Job::getName)
                             .toList();
                 }
 
                 if (args[0].equalsIgnoreCase("removejob")) {
+                    Player target = Bukkit.getPlayer(args[1]);
+                    if (target == null) {
+                        return new ArrayList<>();
+                    }
+                    PlayerData data = RPUniverse.getPlayerData(target.getUniqueId().toString());
                     return RPUniverse.getInstance().getJobsHandler().getJobs().stream()
+                            .filter(job -> data.getPlayerJobs().contains(job))
                             .map(Job::getName)
                             .toList();
                 }
