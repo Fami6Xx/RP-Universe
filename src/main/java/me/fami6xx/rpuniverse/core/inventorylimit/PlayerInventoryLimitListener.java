cec4cb868e4c592b1e590835e3a3879f3b56a557
Fami6Xx <libojer@gmail.com>
2024-12-03 12:01:04 +0100

    feat: update for PlayerInventoryLimitListener (working)

--- src/main/java/me/fami6xx/rpuniverse/core/inventorylimit/PlayerInventoryLimitListener.java
+++ src/main/java/me/fami6xx/rpuniverse/core/inventorylimit/PlayerInventoryLimitListener.java
@@ -1,7 +1,8 @@
 package me.fami6xx.rpuniverse.core.inventorylimit;
 
+import me.fami6xx.rpuniverse.RPUniverse;
 import me.fami6xx.rpuniverse.core.menuapi.types.PaginatedMenu;
-import org.bukkit.Material;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -9,9 +10,27 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryPickupItemEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 
 public class PlayerInventoryLimitListener implements Listener {
+
+    public PlayerInventoryLimitListener() {
+        Bukkit.getScheduler().runTaskTimer(RPUniverse.getJavaPlugin(), () -> Bukkit.getOnlinePlayers().forEach(player -> {
+            for (int i = 0; i < 36; i++) {
+                if (i < 9) {
+                    if (player.getInventory().getItem(i) != null && player.getInventory().getItem(i).isSimilar(PaginatedMenu.BORDER_GLASS)) {
+                        player.getInventory().setItem(i, null);
+                    }
+                }
+                if (i >= 9) {
+                    player.getInventory().setItem(i, PaginatedMenu.BORDER_GLASS);
+                }
+            }
+        }), 0, 5);
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         for (int i = 9; i < 36; i++) {
@@ -21,16 +40,21 @@ public class PlayerInventoryLimitListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getSlot() >= 9 && event.getSlot() < 36) {
+        if (event.getClickedInventory() != null && event.getSlot() >= 9 && event.getSlot() < 36 && event.getClickedInventory() == event.getWhoClicked().getInventory()) {
             event.setCancelled(true);
         }
 
-        if (event.getCurrentItem() != null && event.getCurrentItem().isSimilar(PaginatedMenu.BORDER_GLASS)) event.setCancelled(true);
+        if (event.getCurrentItem() != null && event.getCurrentItem().isSimilar(PaginatedMenu.BORDER_GLASS)) {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
     public void onInventoryDrag(InventoryDragEvent event) {
-        event.getRawSlots().stream().filter(slot -> slot >= 9 && slot < 36).forEach(slot -> event.setCancelled(true));
+        if (event.getInventory() != event.getWhoClicked().getInventory()) return;
+        if (event.getRawSlots().stream().anyMatch(slot -> slot >= 9 && slot < 36)) {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
@@ -49,7 +73,21 @@ public class PlayerInventoryLimitListener implements Listener {
     }
 
     @EventHandler
+    public void onPlayerDropItemEvent(PlayerDropItemEvent event) {
+        if (event.getItemDrop().getItemStack().isSimilar(PaginatedMenu.BORDER_GLASS)) {
+            event.getItemDrop().remove();
+        }
+    }
+
+    @EventHandler
+    public void onPlayerSwapHand(PlayerSwapHandItemsEvent event) {
+        if ((event.getOffHandItem() != null && event.getOffHandItem().isSimilar(PaginatedMenu.BORDER_GLASS)) || (event.getMainHandItem() != null && event.getMainHandItem().isSimilar(PaginatedMenu.BORDER_GLASS))) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
-        event.getDrops().removeIf(item -> item != null && item.getType() == Material.BARRIER);
+        event.getDrops().removeIf(item -> item != null && item.isSimilar(PaginatedMenu.BORDER_GLASS));
     }
 }
