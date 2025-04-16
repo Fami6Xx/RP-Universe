290d57dc1298ed0d3cb9266c9f6e44bc392788d7
Fami6Xx <libojer@gmail.com>
2025-04-16 13:09:44 +0200

    feat: implement update notification system for players and migrate config to version 10
    Signed-off-by: Fami6Xx <libojer@gmail.com>

---
+++ src/main/java/me/fami6xx/rpuniverse/core/misc/UpdateNotificationListener.java
@@ -0,0 +1,49 @@
+package me.fami6xx.rpuniverse.core.misc;
+
+import me.fami6xx.rpuniverse.RPUniverse;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ * Listener for notifying players about available updates when they join the server.
+ */
+public class UpdateNotificationListener implements Listener {
+
+    /**
+     * Handles player join events to notify players with the appropriate permission about available updates.
+     *
+     * @param event The player join event
+     */
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        RPUniverse plugin = RPUniverse.getInstance();
+        
+        // Check if update notifications are enabled
+        if (!plugin.getConfigManager().getConfig().getBoolean("updateNotification.enabled", true)) {
+            return;
+        }
+        
+        // Check if the player has the permission to receive update notifications
+        if (!player.hasPermission("rpu.update.notify")) {
+            return;
+        }
+        
+        // Check if an update is available
+        if (plugin.isUpdateAvailable()) {
+            String currentVersion = VersionInfo.getVersion();
+            String latestVersion = plugin.getLatestVersion();
+            
+            // Send update notification to the player
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', 
+                RPUniverse.getPrefix() + " &cAn update is available! " +
+                "&7Current version: &f" + currentVersion + " &7Latest version: &f" + latestVersion));
+            
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', 
+                RPUniverse.getPrefix() + " &7Please update to the latest version from: &fhttps://polymart.org/resource/rpuniverse.5845"));
+        }
+    }
+}
