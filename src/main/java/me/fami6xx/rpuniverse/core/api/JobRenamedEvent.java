95752cd5918ed45ddba2e0eb31006609ea1e6049
Fami6Xx <libojer@gmail.com>
2025-07-07 14:05:00 +0200

    feat: add `JobRenamedEvent` to API for handling job rename events
    Signed-off-by: Fami6Xx <libojer@gmail.com>

---
+++ src/main/java/me/fami6xx/rpuniverse/core/api/JobRenamedEvent.java
@@ -0,0 +1,28 @@
+package me.fami6xx.rpuniverse.core.api;
+
+import me.fami6xx.rpuniverse.core.jobs.Job;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class JobRenamedEvent extends Event {
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    private final Job job;
+
+    public JobRenamedEvent(Job job) {
+        this.job = job;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+}
